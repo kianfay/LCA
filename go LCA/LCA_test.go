753f7234a2bc080277e576d0dd4fbbee85f7e00d
Kian Fay <kifay@tcd.ie>
2020-10-22 12:36:02 +0100

    Improved go testing

--- go LCA/LCA_test.go
+++ go LCA/LCA_test.go
@@ -1,25 +1,49 @@
 package lca
 
+// Run tests by running 'go test' from tis dir
 import (
 	"testing"
 )
 
+//       7
+//      /  \
+//     4    8
+//    / \  / \
+//   1  5  3  2
+//         \   \
+//         10   6
+//              /
+//             9
+
 /* Tests function must start with "Test" */
 func TestGoLCA(t *testing.T) {
-	root := Node{100, nil, nil}
-	root.left = &Node{50, nil, nil}
-	root.right = &Node{150, nil, nil}
-	root.left.left = &Node{20, nil, nil}
-	root.left.left.left = &Node{10, nil, nil}
-	root.left.left.right = &Node{30, nil, nil}
-	root.left.right = &Node{80, nil, nil}
-	root.right.left = &Node{110, nil, nil}
-	root.right.right = &Node{200, nil, nil}
+	root := Node{7, nil, nil}
+	root.left = &Node{4, nil, nil}
+	root.right = &Node{8, nil, nil}
+	root.left.left = &Node{1, nil, nil}
+	root.left.right = &Node{5, nil, nil}
+	root.right.left = &Node{3, nil, nil}
+	root.right.left.right = &Node{10, nil, nil}
+	root.right.right = &Node{2, nil, nil}
+	root.right.right.right = &Node{6, nil, nil}
+	root.right.right.right.left = &Node{9, nil, nil}
 
 	inorderRecursive(&root)
-	// Assuming that both values n1 & n2 exists in Binary search tree
-	var result = leastCommonAncestorInBST(&root, 10, 80)
-	if result != 50 {
-		t.Error("expected 50, got", result)
+	
+	// Assuming that both values n1 & n2 of all the tests exists in Binary search tree
+	var result = leastCommonAncestorInBST(&root, 1, 5)
+	if result != 4 {
+		t.Error("expected 4, got", result)
 	}
+
+	result = leastCommonAncestorInBST(&root, 1, 8)
+	if result != 7 {
+		t.Error("expected 7, got", result)
+	}
+
+	result = leastCommonAncestorInBST(&root, 4, 8)
+	if result != 7 {
+		t.Error("expected 7, got", result)
+	}
+
 }
